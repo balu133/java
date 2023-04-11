6c5676b265ff64f4a425a411c9e14c63024d6b3c
BalajiBalu <113408435+balu133@users.noreply.github.com>
2023-04-11 21:17:38 +0530

    Update Abstract.java

--- Abstract.java
+++ Abstract.java
@@ -1,1 +1,30 @@
-we are going to see about abstraction in java
+abstract class Shape
+{  
+  double length,breadth;
+  Shape(double length,double breadth)
+  {
+     this.length=length;
+    this.breadth=breadth;
+  }
+  abstract int getArea();
+}
+public class Rectangle extends Shape
+  { 
+     Rectangle(double l,double b)
+     {
+      super(l,b);
+      }
+      
+    public int getArea()
+    {
+    return length*breadth;
+    }
+ }
+ public class Test
+ {
+    public static void main(String[] args)
+    {
+     Rectangle obj=new Rectangle(10,20):
+     System.out.println("The area of the Rectangle is : "+ obj.getArea());
+    }
+ }
